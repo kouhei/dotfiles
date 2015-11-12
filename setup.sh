5c4204ba4e769b1db071ad0112753abcd76e9032
fai1618 <fai1.618033@gmail.com>
2015-11-12 18:10:21 +0900

    change setup.sh
    シンボリックリンクの判定付け足し

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 DOT_FILES=(.vimrc .bashrc .bashAliases .bash_profile .bash)
-
+cd $HOME
 for file in ${DOT_FILES[@]}
 do
-  if [ -e $HOME/$file ]; then
+  if [ -e $file ]; then
     echo "既に存在します: $file"
+  elif [ -n $file ]; then
+    echo "シンボリックリンクです"
   else
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s dotfiles/$file $HOME/$file
     echo "シンボリックリンクを貼りました: $file"
   fi
 done
