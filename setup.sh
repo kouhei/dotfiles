0efcf7f5a94b41b0bd6a38974fda77df69e1f01f
fai1618 <fai1.618033@gmail.com>
2015-11-12 19:11:18 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 DOT_FILES=(.vimrc .bashrc .bashAliases .bash_profile .bash)
-cd $HOME
 for file in ${DOT_FILES[@]}
 do
-  if [ -e $file ]; then
+  if [ -e $HOME/$file ]; then
     echo "既に存在します: $file"
-  elif [ -n $file ]; then
+    cp -r $file $HOME/dotfile$file 
+  elif [ -n $HOME/$file ]; then
     echo "シンボリックリンクです"
+    cp -r $file $HOME/dotfile$file
   else
-    ln -s dotfiles/$file $HOME/$file
+    ln -s $file $HOME/$file
     echo "シンボリックリンクを貼りました: $file"
   fi
 done
