4440d8b4d302d1b58744c204fa16303b375b27d9
fai1618 <fai1.618033@gmail.com>
2015-12-21 01:36:25 +0900

    setup.shを変更
    重複したものがある場合、dotfilesの中に作成

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 DOT_FILES=(.vimrc .bashrc .bashAliases .bash_profile .bash .zshrc .tmux.conf)
+if [ ! -e $HOME/dotfiles ];then
+  mkdir $HOME/dotfiles
+fi
+
 for file in ${DOT_FILES[@]}
 do
   if [ -e $HOME/$file ]; then
     echo "既に存在します: $file"
-    cp -r $file $HOME/dotfile$file
+    cp -r $file $HOME/dotfiles/dotfile$file
   elif [ -L $HOME/$file ]; then
     echo "既に存在します(シンボリックリンクです): $file"
-    cp -r $file $HOME/dotfile$file
+    cp -r $file $HOME/dotfiles/dotfile$file
   else
     ln -s $file $HOME/$file
     echo "シンボリックリンクを貼りました: $file"
