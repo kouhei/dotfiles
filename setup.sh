9e359baa959d682a149580e1b2db80f67187f364
fai1618 <fai1.618033@gmail.com>
2016-01-09 00:34:47 +0900

    fix bug

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
+echo $script_dir
+
 DOT_FILES=(.vimrc .bashrc .bashAliases .bash_profile .bash .zshrc .tmux.conf)
 if [ ! -e $HOME/dotfiles ];then
   mkdir $HOME/dotfiles
@@ -9,12 +12,12 @@ for file in ${DOT_FILES[@]}
 do
   if [ -e $HOME/$file ]; then
     echo "既に存在します: $file"
-    cp -r $file $HOME/dotfiles/dotfile$file
+    #cp -r $file $HOME/dotfiles/dotfile$file
   elif [ -L $HOME/$file ]; then
     echo "既に存在します(シンボリックリンクです): $file"
-    cp -r $file $HOME/dotfiles/dotfile$file
+    #cp -r $file $HOME/dotfiles/dotfile$file
   else
-    ln -s $file $HOME/$file
+    #ln -s $file $HOME/$file
     echo "シンボリックリンクを貼りました: $file"
   fi
 done
