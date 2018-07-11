cff355426eacbd6c6839c5cb7db44d0ea8223ab1
Kouhei Yamada <kouhei@users.noreply.github.com>
2018-07-12 03:55:54 +0900

    deleted:    vimrc.old deleted:    .vim/

--- .vim/template/template.py
+++
@@ -1,7 +0,0 @@
-i#!/usr/bin/env python
-# vim: set fileencoding=utf-8 :
-#
-# Author:   yamada kouhei
-# Created:  <+DATE+>
-#
-<+CURSOR+>
--- .vim/template/template.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-# vim: set fileencoding=utf-8 :
-#
-# Author:   yamada kouhei
-# Created:  <+DATE+>
-#
-<+CURSOR+>
