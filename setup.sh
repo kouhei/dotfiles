9cb3fd4d0b87995c949678ada4ff075b6a9b115f
Kouhei Yamada <kouhei@users.noreply.github.com>
2019-03-03 23:34:36 +0900

    anyenv使うように

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-LANG=C xdg-user-dirs-gtk-update
+#====================================================================
+# 『デスクトップ』『音楽』などの日本語フォルダー名を英語表記にする
+#====================================================================
+env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
 echo $script_dir
@@ -9,6 +12,7 @@ if [ ! -e $HOME/dotfiles ];then
   mkdir $HOME/dotfiles
 fi
 
+echo "================================"
 for file in ${DOT_FILES[@]}
 do
   if [ -e $HOME/$file ]; then
@@ -20,16 +24,35 @@ do
     echo "シンボリックリンクを貼りました: $file"
   fi
 done
+echo "================================"
 
-sudo apt-get install vim tmux zsh curl vlc build-essential openssh-server
+if type apt-get > /dev/null 2>&1; then
+    echo "apt-get exist!"     #コマンドが存在する時の処理
+    echo "apt update & upgrade"
+    yes | sudo apt-get update
+    yes | sudo apt-get upgrade
+    echo "install vim tmux zsh curl vlc build-essential openssh-server"
+    sudo apt-get install -y vim tmux zsh curl vlc build-essential openssh-server
+fi
 
-echo "Install vim-plug..."
+UNAME="$(uname)"
+echo "Install anyenv..."
+if [ $UNAME = 'Darwin' ]; then
+    brew install anyenv
+elif [ $UNAME = 'Linux' ]; then
+    git clone https://github.com/anyenv/anyenv ~/.anyenv
+fi
+
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
+exec $SHELL -l
+
+echo "Install pyenv, nodenv..."
+anyenv install pyenv
+anyenv install nodenv
+
+echo "Install vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-echo "Install pyenv..."
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-echo "Install nodebrew..."
-curl -L git.io/nodebrew | perl - setup
+echo "DONE!"
