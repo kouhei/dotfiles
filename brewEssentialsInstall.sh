084a630808939c527515a733f6d43cb6f4ec9cc0
kouhei <kouhei@users.noreply.github.com>
2022-05-06 21:01:14 +0900

    update

--- brewEssentialsInstall.sh
+++ brewEssentialsInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "install from brew"
-brew install peco git volta mas pure
+brew install peco git volta mas pure gh trash
 # cask
 brew install --cask docker visual-studio-code clipy appcleaner google-chrome deepl notion dropbox spotify rectangle brave-browser typora kindle scroll-reverser krisp malwarebytes karabiner-elements bitwarden
 echo "done"
