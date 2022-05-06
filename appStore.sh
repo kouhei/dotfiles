98f4114881f399ae402640672db6a0f5bbd80d70
kouhei <kouhei@users.noreply.github.com>
2022-05-06 20:57:49 +0900

    update

--- appStore.sh
+++ appStore.sh
@@ -1,1 +1,3 @@
-mas install 407963104 # Pixelmator
+mas install 407963104  # Pixelmator
+mas install 539883307  # LINE
+mas install 1352778147 # bitwarden (safari ext)
--- brewEssentialsInstall.sh
+++ brewEssentialsInstall.sh
@@ -2,5 +2,5 @@
 echo "install from brew"
 brew install peco git volta mas pure
 # cask
-brew install --cask docker visual-studio-code clipy appcleaner google-chrome deepl notion dropbox spotify rectangle brave-browser typora kindle scroll-reverser krisp malwarebytes line karabiner-elements bitwarden
+brew install --cask docker visual-studio-code clipy appcleaner google-chrome deepl notion dropbox spotify rectangle brave-browser typora kindle scroll-reverser krisp malwarebytes karabiner-elements bitwarden
 echo "done"
