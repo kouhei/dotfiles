0b7d39f3c589beaa3ce99d37000b39a89e2c4712
kouhei <kouhei@users.noreply.github.com>
2023-06-11 04:25:10 +0900

    update

--- brewEssentialsInstall.sh
+++ brewEssentialsInstall.sh
@@ -2,5 +2,5 @@
 echo "install from brew"
 brew install peco git volta mas pure gh trash duti wget
 # cask
-brew install --cask docker visual-studio-code clipy appcleaner google-chrome deepl notion dropbox spotify rectangle brave-browser typora kindle scroll-reverser krisp malwarebytes karabiner-elements bitwarden raycast vlc qlmarkdown postman logitech-options
+brew install --cask docker visual-studio-code clipy appcleaner google-chrome deepl notion dropbox spotify rectangle brave-browser typora kindle scroll-reverser krisp malwarebytes karabiner-elements bitwarden raycast vlc qlmarkdown postman logitech-options the-unarciver discord
 echo "done"
